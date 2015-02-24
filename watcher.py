c39911286e23146d46f0af427aef8564157c4c3f
hbc <bcxxxxxx@gmail.com>
2015-02-24 16:18:39 +0800

    Update page layout.

--- watcher.py
+++ watcher.py
@@ -4,8 +4,12 @@ server = Server()
 
 
 def compile_scss():
-    shell('sass --scss %s %s' % ('scss/style.scss', 'css/style.css'))()
-    shell('sass --scss %s %s' % ('scss/print.scss', 'css/print.css'))()
+    shell('sass -t compressed --scss %s %s' % (
+        'scss/style.scss', 'css/style.css'
+    ))()
+    shell('sass -t compressed --scss %s %s' % (
+        'scss/print.scss', 'css/print.css'
+    ))()
 
 
 server.watch('./scss/', compile_scss)
